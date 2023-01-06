e7bb78dc84e4bc6e7fccb62738584cb3dc5bf9a3
도넛맨 <hp196@naver.com>
2023-01-06 16:44:34 +0900

    페이지 컨트롤러, 뷰 추가

---
+++ src/main/java/naver/naver/controller/PageController.java
@@ -0,0 +1,17 @@
+package naver.naver.controller;
+
+import io.swagger.models.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/pages")
+public class PageController {
+
+    @GetMapping("/main")
+    public ModelAndView main() {
+        return new ModelAndView("main");
+    }
+}
